75342a621248ac842340d1ff5f971db19ae504c9
krish <krishnaasinghbisht@gmail.com>
2024-11-09 01:10:20 +0530

    commit-12 79% acc

--- dataScraping/scraping.py
+++ dataScraping/scraping.py
@@ -1,17 +1,20 @@
 import asyncio
 from twikit import Client, TooManyRequests
 from datetime import datetime
+import csv
+from configparser import ConfigParser
 from random import uniform
 
-MINIMUM_TWEETS = 10
-QUERY = 'israel'
+MINIMUM_TWEETS = 30
 MAX_TWEETS_PER_REQUEST = 10
 MIN_DELAY = 5
 MAX_DELAY = 20
 
-username = 'princenegitemp2'
-password = '5013634k'
-email = 'clasherkrishna21@gmail.com'
+config = ConfigParser()
+config.read('config.ini')
+username = config['X']['username']
+password = config['X']['password']
+email = config['X']['email']
 
 async def human_like_delay():
     delay = uniform(MIN_DELAY, MAX_DELAY)
@@ -25,13 +28,17 @@ async def main():
     client.load_cookies('cookies.json')
     tweet_count = 0
     tweets = None
-    alltweets:str = []
-        
+
+    
+    with open('tweets.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Tweet_count','Text'])
+
     while tweet_count < MINIMUM_TWEETS:
         try:
             if tweets is None:
                 print(f'{datetime.now()} - Getting tweets....')
-                tweets = await client.search_tweet(QUERY, product='Media')
+                tweets = await client.search_tweet('narendra modi', product='Top')
             else:
                 print(f'{datetime.now()} - Getting next tweets....')
                 tweets = await tweets.next()
@@ -49,9 +56,11 @@ async def main():
 
                 tweet_count += 1
                 batch_count += 1
-                tweet_data = tweet.text
+                tweet_data = [tweet_count, tweet.text]
 
-                alltweets.append(tweet_data)
+                with open('tweets.csv', 'a', newline='', encoding='utf-8') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(tweet_data)
 
                 await asyncio.sleep(uniform(0.5, 2))
 
@@ -69,7 +78,5 @@ async def main():
 
     print(f'{datetime.now()} - Done! Got {tweet_count} tweets in total')
 
-    return alltweets
-
-asyncio.run(main())
-
+if __name__ == "__main__":
+    asyncio.run(main())
